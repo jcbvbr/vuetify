73dbc4822ccb4a282d2a6935b9a2a66e97b8df47
Julio Veiga <jcbvbr@gmail.com>
2019-11-04 19:59:56 -0300

    Add expansion panel in Projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,10 +1,79 @@
 <template>
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
-  </div>
+
+    <v-container class="my-5">
+      <v-expansion-panels focusable>
+        <v-expansion-panel v-for="project in projects" :key="project.title">
+          <v-expansion-panel-header class="grey--text">{{ project.title }}</v-expansion-panel-header>
+          <v-expansion-panel-content class="px-4 grey--text">          
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project in projects" :key="project.title">
+          <v-expansion-panel-content >
+          <div slot="header">{{ project.title }}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+        </v-expansion-panel>        
+      </v-expansion-panels>
+    </v-container>
+  </div>  
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint velit necessitatibus veritatis a eum nostrum unde exercitationem omnis quidem, eveniet delectus eaque ex ducimus, nulla rerum amet, sed mollitia. Dignissimos.",
+        },
+        {
+          title: "Code Up the homepage",
+          person: "Ching Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint velit necessitatibus veritatis a eum nostrum unde exercitationem omnis quidem, eveniet delectus eaque ex ducimus, nulla rerum amet, sed mollitia. Dignissimos.",
+          
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint velit necessitatibus veritatis a eum nostrum unde exercitationem omnis quidem, eveniet delectus eaque ex ducimus, nulla rerum amet, sed mollitia. Dignissimos.",
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint velit necessitatibus veritatis a eum nostrum unde exercitationem omnis quidem, eveniet delectus eaque ex ducimus, nulla rerum amet, sed mollitia. Dignissimos.",
+        }
+      ]
+    };
+  },
+  computed: {
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === 'The Net Ninja';
+      });
+    }
+  }
 }
 </script>
