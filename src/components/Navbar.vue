ccd7f67466f27f87554e133162e0eb751130c3cf
Julio Veiga <jcbvbr@gmail.com>
2019-11-04 20:24:52 -0300

    Add menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,22 +7,35 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text color="grey" v-on="on">
+            <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer" class="primary">
-        <v-layout column align-center>
-            <v-flex class="mt-5">
-                <v-avatar size="100" class="grey lighten-2">
-                    <img src="/avatar-1.png" alt="">
-                </v-avatar>
-                <p class="white--text subheading mt-1">
-                    The Net Ninja
-                </p>
-            </v-flex>
-        </v-layout>
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100" class="grey lighten-2">
+            <img src="/avatar-1.png" alt />
+          </v-avatar>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
@@ -38,16 +51,16 @@
 </template>
 
 <script>
-    export default {
-    data() {
-        return {
-        drawer: false,
-        links: [
-            { icon: "dashboard", text: "Dashboard", route: "/" },
-            { icon: "folder", text: "My Projects", route: "/projects" },
-            { icon: "person", text: "Team", route: "/team" }
-        ]
-        };
-    }
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
     };
+  }
+};
 </script>
